c58fba78efd6ef4ac355d5d3cec0b647dbc9f0a8
modolee <lsi1318@gmail.com>
2018-07-12 16:17:17 +0900

    예제 따라하기

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,97 +6,94 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import ecommerce_store_artifacts from '../../build/contracts/EcommerceStore.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+var EcommerceStore = contract(ecommerce_store_artifacts);
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
+const ipfsAPI = require('ipfs-api');
+const ethUtil = require('ethereumjs-util');
+
+const ipfs = ipfsAPI({host: '54.242.168.42', port: '5001', protocol: 'http'});
 
 window.App = {
-  start: function() {
-    var self = this;
+ start: function() {
+  var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+  EcommerceStore.setProvider(web3.currentProvider);
+  renderStore();
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
+  var reader;
+  $("#product-image").change(function(event) {
+    const file = event.target.files[0]
+    reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+  });
+ },
 
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
-
-      accounts = accs;
-      account = accounts[0];
-
-      self.refreshBalance();
-    });
-  },
-
-  setStatus: function(message) {
-    var status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-
-  refreshBalance: function() {
-    var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
-  },
-
-  sendCoin: function() {
-    var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
-    });
-  }
 };
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-  }
+ // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+ //if (typeof web3 !== 'undefined') {
+ // console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+  // Use Mist/MetaMask's provider
+//  window.web3 = new Web3(web3.currentProvider);
+// } else {
+  console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+  // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+  window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// }
 
-  App.start();
+ App.start();
 });
+
+function renderStore() {
+ EcommerceStore.deployed().then(function(i) {
+  i.getProduct.call(1).then(function(p) {
+   $("#product-list").append(buildProduct(p));
+  });
+  i.getProduct.call(2).then(function(p) {
+   $("#product-list").append(buildProduct(p));
+  });
+ });
+}
+
+function buildProduct(product) {
+ let node = $("<div/>");
+ node.addClass("col-sm-3 text-center col-margin-bottom-1");
+ node.append("<img src='https://ipfs.io/ipfs/" + product[3] + "' width='150px' />");
+ node.append("<div>" + product[1]+ "</div>");
+ node.append("<div>" + product[2]+ "</div>");
+ node.append("<div>" + product[5]+ "</div>");
+ node.append("<div>" + product[6]+ "</div>");
+ node.append("<div>Ether " + product[7] + "</div>");
+ return node;
+}
+
+function saveImageOnIpfs(reader) {
+ return new Promise(function(resolve, reject) {
+  const buffer = Buffer.from(reader.result);
+  ipfs.add(buffer)
+  .then((response) => {
+   console.log(response)
+   resolve(response[0].hash);
+  }).catch((err) => {
+   console.error(err)
+   reject(err);
+  })
+ })
+}
+
+function saveTextBlobOnIpfs(blob) {
+ return new Promise(function(resolve, reject) {
+  const descBuffer = Buffer.from(blob, 'utf-8');
+  ipfs.add(descBuffer)
+  .then((response) => {
+   console.log(response)
+   resolve(response[0].hash);
+  }).catch((err) => {
+   console.error(err)
+   reject(err);
+  })
+ })
+}
